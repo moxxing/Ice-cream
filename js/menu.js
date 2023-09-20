4a23ef223c1ad7d07781c7091ba961ecc2bfd8c0
moxxing <vladdeman4ik@gmail.com>
2023-09-20 22:20:06 +0300

    5

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,10 @@
     q.addEventListener("click", () => {
         const expanded = q.getAttribute("aria-expanded") === "true" || false;
 
-        q.classList.toggle("is-open")
+        q.classList.toggle("is-open");
+        e.classList.toggle("is-open-menu");
+        q.setAttribute("aria-expanded", !expanded);
 
-    }
-     )
-})
+        w.classList.toggle("is-open");
+    });
+})();
